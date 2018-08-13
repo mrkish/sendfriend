f33cb8fbe71e31f9db001f77727b0cf598d8e77f
Mark Johnson <mark@jhns.in>
2018-08-12 21:41:26 -0500

    Adding Cookies for login management

--- src/main/java/com/sendfriend/controllers/UserController.java
+++ src/main/java/com/sendfriend/controllers/UserController.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.Cookie;
 import javax.validation.Valid;
 import java.util.List;
 
@@ -25,7 +27,7 @@ public class UserController {
     RouteDao routeDao;
 
     @RequestMapping(value = "")
-    public String index(Model model) {
+    public String index(Model model, @CookieValue(value = "user", defaultValue =  "none")) {
 
         model.addAttribute("title", "Sendfriend! | Index");
         model.addAttribute("users",userDao.findAll());
@@ -61,13 +63,23 @@ public class UserController {
     @RequestMapping(value = "login", method = RequestMethod.GET)
     public String displayLogin(Model model) {
 
+
         model.addAttribute("title", "Sendfriend | Login!");
 
         return "user/login";
     }
 
     @RequestMapping(value = "login", method = RequestMethod.POST)
-    public String processLogin(Model model) {
+    public String processLogin(Model model, @ModelAttribute @Valid User user) {
+        List<User> userExists = userDao.findByUsername(user.getUsername());
+        if (userExists.isEmpty()) {
+            model.addAttribute("title", "Sendfriend | Login!");
+            model.addAttribute("errors", "Login failed");
+        }
+        User loggedIn = userExists.get(0);
+        if (loggedIn.getPassword().equals(user.getPassword())) {
+            Cookie c = new Cookie("user",user.getUsername());
+        }
 
         return "redirect:index/";
     }
