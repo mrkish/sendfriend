5038417c419aa700986e026bcee99a51cc3ffc69
mrkish <mark@jhns.in>
2018-08-21 09:08:00 -0500

    Added controller annotations for Crag and Area Controllers

--- src/main/java/com/sendfriend/controllers/CragController.java
+++ src/main/java/com/sendfriend/controllers/CragController.java
@@ -1,4 +1,9 @@
 package com.sendfriend.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "crag")
 public class CragController {
 }
