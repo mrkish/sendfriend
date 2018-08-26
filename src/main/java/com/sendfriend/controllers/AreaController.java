d06d454c3f45c6b17139921ece8c26968450d613
mrkish <mark@jhns.in>
2018-08-25 22:54:38 -0500

    Added Area view and index handler

--- src/main/java/com/sendfriend/controllers/AreaController.java
+++ src/main/java/com/sendfriend/controllers/AreaController.java
@@ -1,9 +1,28 @@
 package com.sendfriend.controllers;
 
+import com.sendfriend.models.data.AreaDao;
+import com.sendfriend.models.data.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(value = "area")
 public class AreaController {
+
+    @Autowired
+    AreaDao areaDao;
+
+    @Autowired
+    UserDao userDao;
+
+    @RequestMapping(value = "")
+    public String index(Model model) {
+
+        model.addAttribute("title", "Areas");
+        model.addAttribute("areas", areaDao.findAll());
+
+        return "area/index";
+    }
 }
