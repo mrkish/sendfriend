a540edb0bb2222a0d6ba9a5ee162ce07461f2027
ahoythar@gmail.com <ahoythar@gmail.com>
2019-05-04 10:59:36 -0500

    Cleaned up RouteService

--- src/main/java/com/sendfriend/service/RouteService.java
+++ src/main/java/com/sendfriend/service/RouteService.java
@@ -1,29 +1,29 @@
 package com.sendfriend.service;
 
-import com.sendfriend.models.Area;
-import com.sendfriend.models.Route;
 import com.sendfriend.data.AreaDao;
 import com.sendfriend.data.CragDao;
 import com.sendfriend.data.RouteDao;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.sendfriend.models.Area;
+import com.sendfriend.models.Route;
 
 import java.util.List;
 
 public class RouteService {
 
-    @Autowired
     RouteDao routeDao;
-
-    @Autowired
     CragDao cragDao;
-
-    @Autowired
     AreaDao areaDao;
+
+    public RouteService(RouteDao routeDao,
+                        CragDao cragDao,
+                        AreaDao areaDao) {
+        this.routeDao = routeDao;
+        this.cragDao = cragDao;
+        this.areaDao = areaDao;
+    }
 
     public void addAndAssociateRoute(Route route, String areaName) {
 
         List<Area> area = areaDao.findByName(areaName);
-
-
     }
 }
